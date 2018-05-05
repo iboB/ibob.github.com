ab4cf45a7b6c21ed64bf93e587a301d20473117f
Borislav Stanimirov <b.stanimirov@abv.bg>
2018-05-05 17:15:37 +0300

    code to handle state presets from reveal fragments

--- slides/code4gpus-bg/js/demo.js
+++ slides/code4gpus-bg/js/demo.js
@@ -1,5 +1,88 @@
 var WebGLDemo = (function() {
     var canvas = null;
+    var gl = null;
+    var requestAnimationFrameId = null;
+    var prevFrameTime = 0;
+
+    var state = {
+        dt: 0,
+    }
+
+    var statePresetStack = [];
+
+    function initializeGL() {
+        gl = canvas.getContext('webgl');
+        if (!gl) {
+          alert('o_O : Unable to init WebGL!');
+          return;
+        }
+
+        gl.clearColor(0.8, 1, 0.8, 1);
+        gl.clearDepth(1.0);
+        gl.enable(gl.DEPTH_TEST);
+        gl.depthFunc(gl.LEQUAL);
+        gl.enable(gl.CULL_FACE);
+        gl.frontFace(gl.CW);
+    }
+
+    function isAnimating() {
+        return requestAnimationFrameId !== null;
+    }
+
+    function frame(now) {
+        now *= 0.001; // to seconds
+
+        state.dt = now - prevFrameTime;
+        prevFrameTime = now;
+
+        draw();
+
+        requestAnimationFrameId = requestAnimationFrame(frame);
+    }
+
+    function draw() {
+        gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    }
+
+    function pushStatePreset(name) {
+        if (!isAnimating()) {
+            // not in demo mode... so don't care about this
+            return;
+        }
+
+        if (setStatePreset(name)) {
+            statePresetStack.push(name);
+        }
+    }
+
+    function popStatePreset(name) {
+        if (!isAnimating()) {
+            // not in demo mode... so don't care about this
+            return;
+        }
+
+        if (statePresetStack.length > 0 && statePresetStack[statePresetStack.length - 1] === name) {
+            statePresetStack.pop();
+        } else {
+            console.log('Unknown state:', name);
+            return;
+        }
+
+        if (statePresetStack.length == 0) {
+            // either popping something we don't seem to have here o_O
+            // or popping the last thing
+            // set empty state and hope for the best
+            setStatePreset('empty');
+        } else {
+            // set the new top as current
+            setStatePreset(statePresetStack[statePresetStack.length - 1]);
+        }
+    }
+
+    function setStatePreset(name) {
+        console.log('Setting preset to', name);
+        return true;
+    }
 
 //////////////////////////
 // Hooks
@@ -14,16 +97,33 @@ var WebGLDemo = (function() {
                 canvas = document.createElement('canvas');
                 canvas.className = 'webgl';
                 holder.appendChild(canvas);
+
+                initializeGL();
+            }
+
+            // start or resume animation if it's not running
+            if (!isAnimating()) {
+                prevFrameTime = performance.now();
+                requestAnimationFrameId = requestAnimationFrame(frame);
+            }
+        } else {
+            // no canvas on this slide so stop animation (if we have it)
+            if (isAnimating()) {
+                cancelAnimationFrame(requestAnimationFrameId);
+                requestAnimationFrameId = null;
             }
         }
+
+        // in any case clear the state preset stack
+        statePresetStack.length = 0;
     }
 
     function onNewFragment(fragment) {
-        console.log('On new fragment:', fragment.innerText);
+        pushStatePreset(fragment.innerText);
     }
 
     function onFragmentHidden(fragment) {
-        console.log('On hidden:', fragment.innerText);
+        popStatePreset(fragment.innerText);
     }
 
     return {
